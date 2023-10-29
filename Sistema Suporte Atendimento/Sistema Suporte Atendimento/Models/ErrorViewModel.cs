b3327d7ba1b9e8cc18303e22ef5202bb672ffbfa
samuV1 <samuel.emailprofissional@gmail.com>
2023-10-29 15:31:31 -0300

    Criação do projeto do sistema

---
+++ Sistema Suporte Atendimento/Sistema Suporte Atendimento/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sistema_Suporte_Atendimento.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
